3c94fa326313d5ed2076dda5285c18214e0cc83c
Bill Konchellah <williamoriginal2019@gmail.com>
2021-09-13 19:23:21 +0300

    "Backend PUT"

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React,{ useState } from 'react'
+import React,{ useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note')
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(()=> {
+    console.log('effect')
+    axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+  },[])
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
-    const noteObject ={
+    const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
-      important: Math.randomn() < 0.5,
-      id: notes.length + 1,
+      important: Math.random() < 0.5,
+    
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+   axios
+   .post('http://localhost:3001/notes',noteObject)
+   .then(response => {
+     setNotes(notes.concat(response.data))
+     setNewNote('')
+   })
   }
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
+
+  const notesToShow = showAll
+    ?notes
+    :notes.filter(note => note.important === true)
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
             <Note key={note.id} note={note} />
         )}
       </ul>
